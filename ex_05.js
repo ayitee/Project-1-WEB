025b4e1bf7b8f02037116c55cf3e071dc6122345
ayitee <matis.gaudin@epitech.digital>
2024-09-30 11:34:05 +0200

    Update ex_05.js

--- ex_05.js
+++ ex_05.js
@@ -2,9 +2,10 @@ const firstname = "John"
 const lastname = "Doe"
 const cardholderFirstName = "John"
 const cardholderLastName = "Doe"
-const cardNumber = "1234 5678 9012 3456"
-const expiryDate = "12/2025"
+const cardNumber = "1111 2222 3333 4444"
+const expiryDateMonth = "21"
+const expiryDateYear = "2042"
 
-let computedBankInfo = cardholderFirstName + "," + cardholderLastName + "," + cardNumber + "," + expiryDate
+let computedBankInfo = cardholderFirstName + "," + cardholderLastName + "," + cardNumber + "," + expiryDateMonth + "," + expiryDateYear + 
 
 displayInCard(computedBankInfo);
