d167c47aebf1696921f5d466a84489c2e43e9cd6
ayitee <matis.gaudin@epitech.digital>
2024-09-30 13:49:07 +0200

    Update ex_08.js

--- ex_08.js
+++ ex_08.js
@@ -7,3 +7,10 @@ if (breadCount >= 2 && hamCount >= 1 && tunaCount >= 1) {
 } else {
   displayThisText("I'd rather be fasting tonight...");
 }
+
+
+Voici des images qui marchent actuellement 
+
+"https://www.boulangerielangelus.com/wp-content/uploads/2020/09/PAIN-COMPLET-MD-DET-1024x683.png",
+"https://www.fernand-dussert.fr/media/catalog/product/cache/5/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/j/a/jambonblanc-02.png",
+"https://img.over-blog-kiwi.com/1/04/49/45/20200612/ob_6c6ac9_big-thon.jpg"
