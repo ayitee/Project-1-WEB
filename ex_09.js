c876550e190f2dd62576b5086c86375c8862823d
ayitee <matis.gaudin@epitech.digital>
2024-09-30 15:18:12 +0200

    Update ex_09.js

--- ex_09.js
+++ ex_09.js
@@ -1,10 +1,8 @@
-displayThisText("The variable value is" + input_var)
+displayThisText("The variable value is " + input_var)
 if (Number.isInteger(input_var)) {
-  displayThisText("Its type is integer") }
-else if (Number.isFinite(input_var)) {
-  displayThisText("Its type is" + ) }
-else {
-  displayThisText("Its type is integer") }
-if input_var == 42) {
-  displayThisText("It is the meaning of life.") }
-
+displayThisText("Its type is integer") }
+else if (Number. isFinite(input_var)) {
+displayThisText("Its type is float"); }
+else displayThisText("Its type is " + typeof input_var)
+if (input_var == 42) [
+displayThisText("It is the meaning of life.");
