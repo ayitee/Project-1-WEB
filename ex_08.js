8eae8fafbf421015e04ed29277226240c3684f29
ayitee <matis.gaudin@epitech.digital>
2024-09-30 13:47:22 +0200

    Update ex_08.js

--- ex_08.js
+++ ex_08.js
@@ -1,16 +1,9 @@
-let breadCount = Math.floor(Math.random() * 4);
-let hamCount = Math.floor(Math.random() * 3);
-let tunaCount = Math.floor(Math.random() * 3);
-
 if (breadCount >= 2 && hamCount >= 1 && tunaCount >= 1) {
   displayThisText("I can make a royal sandwich!");
-  
 } else if (breadCount >= 2 && hamCount >= 1) {
   displayThisText("I can make a ham sandwich!");
-  
 } else if (breadCount >= 2 && tunaCount >= 1) {
   displayThisText("I can make a tuna sandwich!");
-  
 } else {
   displayThisText("I'd rather be fasting tonight...");
 }
